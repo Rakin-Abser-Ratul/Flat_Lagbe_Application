d54a1a9cfcf43a16fd8f43f21fbbc0ceed25722d
Rakin-Abser-Ratul <raratul15@gmail.com>
2026-07-22 23:26:22 +0600

    Update project files for deployment

--- backend-django/Flat_Lagbe_Application/Flat_Lagbe_Application/settings.py
+++ backend-django/Flat_Lagbe_Application/Flat_Lagbe_Application/settings.py
@@ -40,7 +40,10 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = []
+# settings.py
+
+# Allow local dev + Render host dynamically
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost 127.0.0.1').split()
 
 
 # Application definition
